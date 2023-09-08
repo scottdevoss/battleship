0190a4d71bf5feb30796bea67737f9cd9af3f953
Scott DeVoss <scottdevoss@yahoo.com>
2023-09-07 21:28:57 -0600

    feat: added empty and place_ship methods

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -13,4 +13,20 @@ RSpec.describe Cell do
     end
   end
 
+  describe '#ship' do 
+    it 'can have a ship or nothing' do
+      expect(@cell.ship).to be nil
+    end
+  end
+
+  describe '#empty?' do
+    it 'can check if a ship cell is empty or not' do
+      expect(@cell.empty?).to be true
+      cruiser = Ship.new("Cruiser", 3)
+      @cell.place_ship(cruiser)
+      @cell.ship
+      expect(@cell.empty?).to be false
+    end
+  end
+
 end
