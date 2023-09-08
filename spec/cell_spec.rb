a567a36014be5f5ef7a26875bdf931498463aa65
Scott DeVoss <scottdevoss@yahoo.com>
2023-09-07 21:07:25 -0600

    feat: added Cell class and initialize method and test

---
+++ spec/cell_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require './lib/ship'
+require './lib/cell'
+
+RSpec.describe Cell do
+  before(:each) do
+    @cell = Cell.new("B4")
+  end
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@cell).to be_an_instance_of(Cell)
+    end
+  end
+
+end
