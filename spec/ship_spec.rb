a4dbc13e55142069a185ca333154465a9e70f29f
Scott DeVoss <scottdevoss@yahoo.com>
2023-09-07 19:39:46 -0600

    feat: added sunk? and hit method in Ship class

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Ship do
     end
   end
 
+  describe '#sunk?' do
+    it 'can determine if a ship is sunk or not' do
+      cruiser = Ship.new("Cruiser", 3)
+      expect(cruiser.sunk?). to be false
+    end
+  end
+
+  describe '#hit' do
+    it 'can hit and damage a ship' do
+      cruiser = Ship.new("Cruiser", 3)
+      cruiser.hit
+      expect(cruiser.health).to eq(2)
+    end
+  end
+
 end
