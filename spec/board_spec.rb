6550b4f5efed31773f75b72a3d07c5592fd339be
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2023-09-08 17:58:53 -0400

    Readjusted describe statement

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,13 +9,17 @@ RSpec.describe Board do
     @board = Board.new
   end
 
-  it 'exists'  do 
-    expect(@board).to be_an_instance_of(Board)
-  end
-
-  it 'has cells for 4X4 board' do 
-    expect(@board.cells.values.first).to be_an(Cell)
-    expect(@board.cells).to be_a(Hash)
-    expect(@board.cells.size).to eq(16)
+  describe '#initialize' do
+    it 'exists'  do 
+      expect(@board).to be_an_instance_of(Board)
+    end
   end 
-end
+
+  describe 'has attributes' do
+    it 'has cells for 4X4 board' do 
+      expect(@board.cells.values.first).to be_an(Cell)
+      expect(@board.cells).to be_a(Hash)
+      expect(@board.cells.size).to eq(16)
+    end 
+  end
+end 
