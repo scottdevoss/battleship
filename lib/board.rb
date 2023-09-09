79e559be85ee86896bf40f48170b9891b6b0604c
Scott DeVoss <scottdevoss@yahoo.com>
2023-09-09 12:55:13 -0600

    feat: made changes to valid placement method

--- lib/board.rb
+++ lib/board.rb
@@ -1,106 +1,58 @@
 class Board 
 attr_reader :cells
+
   def initialize
-    @cells = {
-        "A1" => Cell.new("A1"),
-        "A2" => Cell.new("A2"),
-        "A3" => Cell.new("A3"),
-        "A4" => Cell.new("A4"),
-        "B1" => Cell.new("B1"),
-        "B2" => Cell.new("B2"),
-        "B3" => Cell.new("B3"),
-        "B4" => Cell.new("B4"),
-        "C1" => Cell.new("C1"),
-        "C2" => Cell.new("C2"),
-        "C3" => Cell.new("C3"),
-        "C4" => Cell.new("C4"),
-        "D1" => Cell.new("D1"),
-        "D2" => Cell.new("D2"),
-        "D3" => Cell.new("D3"),
-        "D4" => Cell.new("D4")
-      }
+    @cells = create_cells
+    @letter = ("A".."D").to_a
+    @number = (1..4).to_a
+  end 
+
+  def create_cells 
+    cells = {}
+    ("A".."D").each do |letter|
+      (1..4).each do |number|
+        coordinate = "#{letter}#{number}"
+        cells[coordinate] = Cell.new(coordinate)
+      end
+    end
+      cells
   end
 
   def valid_coordinate?(coordinate)
-    if @cells.keys.include?(coordinate)
-      true
-    else 
-      false
-    end
+    @cells.key?(coordinate)
   end
 
   def valid_placement?(ship, coordinates)
+    return false unless ship.length == coordinates.count
+    return false unless direction_placement?(coordinates)
+    return false unless horizontal_consecutive?(coordinates) || vertical_consecutive?(coordinates)
+    return false if overlap?(coordinates)
+    true
+  end 
 
-    #check length of ship 
-    ship_length = ship.length
-    #check number of coordinates
-    coordinates_length = coordinates.length
-    #make sure they are both equal
-    
-    numbers = []
-    letters = []
-    coordinates.each do |cord|
-      numbers << cord.chars.last.to_i
-      letters << cord.chars.first
-      
-    end
-    
-    if ship_length == coordinates_length
-      
-      check_letters(letters)
-      check_numbers(numbers)
-     
-      if check_letters(letters) == false || check_numbers(numbers) == false
-        check_diagonal(coordinates)
-      elsif check_letters(letters) == true && check_numbers(numbers) == true
-        true
-      end 
-      
-    else 
-      false
-    end 
+  def direction_placement?(coordinates)
+    letter = coordinates.map { |coordinate| coordinate[0] }
+    number =  coordinates.map { |coordinate| coordinate[1..-1].to_i }
+    letter.uniq.length == 1 || number.uniq.length == 1
+  end 
+
+  def horizontal_consecutive?(coordinates)
+    letter = coordinates.map { |coordinate| coordinate[0] }
+    number =  coordinates.map { |coordinate| coordinate[1..-1].to_i }
+
+    letter.uniq.length == 1 && number.each_cons(2).all? { |key, value| (value - key) == 1 }
+    # returns false when the placement is NOT consecutive
   end
 
-  def check_letters(letters)
-    sorted_letters = letters.sort
-    sorted_letters.uniq.count == 1 || sorted_letters.each_cons(2).all?{|a,b| b.ord - a.ord == 1} 
+  def vertical_consecutive?(coordinates)
+    letter = coordinates.map { |coordinate| coordinate[0] }
+    number =  coordinates.map { |coordinate| coordinate[1..-1].to_i }
+
+    number.uniq.length == 1 && letter.each_cons(2).all? { |key, value| (value.ord - key.ord) == 1 }
+    # returns false when the placement is NOT consecutive
   end
 
-  def check_numbers(numbers)
-   
-    # sorted_numbers = numbers.sort 
-    numbers.each_cons(2).all?{|a,b| b - a == 1} 
-
-  end
-
-  def check_diagonal(coordinates)
-    #if letters are different then the numbers have to be the same
-    # if letters.uniq.count > 1
-    #   numbers.uniq.count == 1
-    # elsif numbers.uniq.count == 1
-    #       letters.uniq.count > 1
-    # else  
-    #   true
-    # end
-    # letters.uniq.length > 1 && numbers.uniq.length > 1
-    # letters = coordinates.map { |coordinate| coordinate[0].ord }
-    # numbers = coordinates.map { |coordinate| coordinate[1..-1].to_i }
-  
-    # diffs = []
-    # coordinates.each_with_index do |coordinate, idx|
-    #   diffs << (letters[idx] - numbers[idx])
-    # end
-  
-    # letters.uniq.length > 1 && numbers.uniq.length > 1 && diffs.uniq.length == 1
-
-
-    letters = coordinates.map { |coordinate| coordinate[0].ord }
-    numbers = coordinates.map { |coordinate| coordinate[1..-1].to_i }
-
-    letters.uniq.length > 1 && numbers.uniq.length > 1
-
-
-    #if the numbers are the same the letters have to be different
-
-  end
+  def overlap?(coordinates)
+    coordinates.any? { |coordinate| @cells[coordinate].ship != nil }
+  end 
 end
